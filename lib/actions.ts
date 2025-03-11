689473eee02e95b23e665ddec597fcf5ed7cf0e6
Diego Torres <diegotc86@gmail.com>
2025-03-11 12:47:17 -0500

    Simplified addToCart function

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,11 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import {
-  createOrUpdateCartItem,
-  fetchCartItemsCount,
-  fetchOrCreateCart,
-} from "./data";
+import { addToCart, fetchCartItemsCount } from "./data";
 import { cookies } from "next/headers";
 import { v4 as uuidv4 } from "uuid";
 
@@ -32,9 +28,7 @@ export async function addItemToCart(formData: FormData) {
       });
     }
 
-    // Data layer only handles database operations
-    const cart = await fetchOrCreateCart(sessionId);
-    await createOrUpdateCartItem(cart.id, productId, quantity);
+    await addToCart(sessionId, productId, quantity);
 
     // Revalidate related pages
     revalidatePath("/", "layout");
--- lib/data.ts
+++ lib/data.ts
@@ -91,47 +91,32 @@ export async function fetchProductById(id: number): Promise<Product | null> {
   }
 }
 
-// Fetch cart or create it if not exists
-export async function fetchOrCreateCart(sessionId: string) {
-  try {
-    const start = Date.now();
-
-    const data = await sql<Cart[]>`
-      SELECT * FROM carts WHERE session_id = ${sessionId}
-    `;
-
-    let cart = data[0];
-    if (!cart) {
-      const data = await sql<Cart[]>`
-      INSERT INTO carts (user_id, session_id)
-      VALUES (NULL, ${sessionId})
-      RETURNING *`;
-
-      cart = data[0];
-    }
-
-    const end = Date.now();
-    const time = end - start;
-    console.log(`Cart data fetched in ${time}ms`);
-    return cart;
-  } catch (error) {
-    console.error("Database Error:", error);
-    throw new Error("Failed to fetch or create cart.");
-  }
-}
-
 // Create or Update cart item
-export async function createOrUpdateCartItem(
-  cartId: number,
+export async function addToCart(
+  sessionId: string,
   productId: number,
   quantity: number
 ) {
   try {
     const start = Date.now();
 
+    const data = await sql<Cart[]>`
+    SELECT * FROM carts WHERE session_id = ${sessionId}
+  `;
+
+    let cart = data[0];
+    if (!cart) {
+      const data = await sql<Cart[]>`
+        INSERT INTO carts (user_id, session_id)
+        VALUES (NULL, ${sessionId})
+        RETURNING *`;
+
+      cart = data[0];
+    }
+
     await sql`
       INSERT INTO cart_items (cart_id, product_id, quantity)
-      VALUES (${cartId}, ${productId}, ${quantity})
+      VALUES (${cart.id}, ${productId}, ${quantity})
       ON CONFLICT (cart_id, product_id) 
       DO UPDATE SET quantity = cart_items.quantity + EXCLUDED.quantity
     `;
